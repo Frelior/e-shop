e6bb73f37a5100b091ce0042b570c4241278ce35
Michael Rudenko <annoyedmichael@gmail.com>
2024-08-06 21:58:25 +0300

    header markup

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,42 @@
+import Link from "next/link"
+import styles from "./Header.module.scss"
+import NavLink from "../NavLink/NavLink"
+
 const Header = () => {
   return (
-    <div className="container">
-      <h1>Header</h1>
-    </div>
+    <header className={`${styles.outer}`}>
+      <div className={`container ${styles.inner}`}>
+        <Link
+          href="/"
+          className={styles.logo}
+        >
+          E-Shop
+        </Link>
+        <div className={styles["input-wrapper"]}>
+          <input
+            type="text"
+            placeholder="Search"
+          />
+        </div>
+        <nav>
+          <NavLink
+            href="/catalog"
+            title="Catalog"
+            imageSrc="/icons/icon_catalog.svg"
+          />
+          <NavLink
+            href="/cart"
+            title="Cart"
+            imageSrc="/icons/icon_cart.svg"
+          />
+          <NavLink
+            href="/profie"
+            title="Profie"
+            imageSrc="/icons/icon_profile.svg"
+          />
+        </nav>
+      </div>
+    </header>
   )
 }
 
---
+++ components/NavLink/NavLinkInterfaces.ts
@@ -0,0 +1,5 @@
+export interface INavLink {
+  title: string
+  href: string
+  imageSrc: string | null
+}
